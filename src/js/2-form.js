4b07f0efd9bef5b19f80fb351f84cb7b3cc09b78
Svitlana <lanamartinenko180@gmail.com>
2026-04-17 19:47:14 +0300

    save

---
+++ src/js/2-form.js
@@ -0,0 +1,36 @@
+const formData = {
+  email: "",
+  message: ""
+};
+
+const form = document.querySelector(".feedback-form");
+
+form.addEventListener("input", (event) => {
+  formData[event.target.name] = event.target.value.trim();
+  localStorage.setItem("feedback-form-state", JSON.stringify(formData));
+});
+
+const savedData = localStorage.getItem("feedback-form-state");
+if (savedData) {
+  const parsedData = JSON.parse(savedData);
+  formData.email = parsedData.email || "";
+  formData.message = parsedData.message || "";
+  form.elements.email.value = formData.email;
+  form.elements.message.value = formData.message;
+}
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  if (!formData.email || !formData.message) {
+    alert("Fill please all fields");
+    return;
+  }
+
+  console.log(formData);
+
+  localStorage.removeItem("feedback-form-state");
+  formData.email = "";
+  formData.message = "";
+  form.reset();
+});
