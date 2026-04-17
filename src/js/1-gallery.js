4761b6a35871cc0e32bda348bc14f352080131aa
Svitlana <lanamartinenko180@gmail.com>
2026-04-17 20:02:23 +0300

    save

--- src/js/1-gallery.js
+++ src/js/1-gallery.js
@@ -1,6 +1,6 @@
-// Описаний в документації
+
 import SimpleLightbox from "simplelightbox";
-// Додатковий імпорт стилів
+
 import "simplelightbox/dist/simple-lightbox.min.css";
 
 const images = [
@@ -88,4 +88,10 @@ const markup = images
   .join('');
 
 gallery.insertAdjacentHTML('beforeend', markup);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+
 
